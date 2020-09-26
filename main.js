52aab63e56917ba520207c07a1fae307b94060cf
SanyuktaKhetan <sanyuktakhetan123@gmail.com>
2020-09-27 01:48:45 +0530

    Delete main.js

--- main.js
+++
@@ -1,7 +0,0 @@
-function changecolor(){
-    
-document.getElementById("head1").style.color="#9100ab";
-}
-function changeimg(){
-    document.getElementById("img1").src="https://images-na.ssl-images-amazon.com/images/I/81SVOMzaHWL._SL1280_.jpg";
-}
